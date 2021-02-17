7b2b72da6573cc684691eae148c1715b306c0869
AlexMartin23 <832007@mystma.org>
2021-02-17 16:03:43 +0000

    plantes try 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def planets(p):
-    planets = ["murcury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+    planet = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
     for i in range (0, 8):
-        if p == planets[i]:
+        if p == planet[i]:
             return i + 1
     return p + " is not a planet"
 
